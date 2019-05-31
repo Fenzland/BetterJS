24c757d786812cf312739f75d080715548718254
yeyuheng <yeyuheng@qianchengtec.com>
2019-05-31 18:54:16 +0800

    add Math.mod() function

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import './src/list-extend-array.js';
 import './src/promisive-timeout-and-animation-frame.js';
 import './src/string-prototype/matchGroup.js';
 import './src/array-prototype/findLastIndex-and-findLast.js';
+import './src/math.js';
---
+++ src/math.js
@@ -0,0 +1,10 @@
+
+/**
+ * The correct modulo. Works like % in Python rather than C.
+ * 
+ * @param dividend (number)
+ * @param divisor  (number)
+ * 
+ * @return (number)
+ */
+Math.mod= ( dividend, divisor, )=> (dividend%divisor - - divisor)%divisor;
