ab9b4c95bb6919b0a949d2d80e74c34f4abbf3ca
Fenz <uukooo@126.com>
2019-06-02 12:53:44 +0800

    fix ref error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './src/list-extend-array.js';
+import './src/lists-extend-array.js';
 import './src/promisive-timeout-and-animation-frame.js';
 import './src/string-prototype/matchGroup.js';
 import './src/array-prototype/findLastIndex-and-findLast.js';
