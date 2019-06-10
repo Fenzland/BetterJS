cd1d59341a8c014e85eaff4365bc99f071e923cb
Fenz <uukooo@126.com>
2019-06-10 23:37:49 +0800

    fix Array prototype

--- src/array-prototype/findLastIndex-and-findLast.js
+++ src/array-prototype/findLastIndex-and-findLast.js
@@ -1,17 +1,21 @@
 
-Array.prototype.findLastIndex= function( predicate, context, ){
-	
-	for( let I= this.length; --I >= 0; )
-	{
-		const predication= predicate.call( context, this[I], I, this, );
+Object.defineProperty( Array.prototype, 'findLastIndex', {
+	value( predicate, context, ){
 		
-		if( predication )
-			return I;
-	}
-	
-	return -1;
-};
+		for( let I= this.length; --I >= 0; )
+		{
+			const predication= predicate.call( context, this[I], I, this, );
+			
+			if( predication )
+				return I;
+		}
+		
+		return -1;
+	},
+}, );
 
-Array.prototype.findLast= function( predicate, context, ){
-	return this[this.findLastIndex( predicate, context, )];
-};
+Object.defineProperty( Array.prototype, 'findLast', {
+	value( predicate, context, ){
+		return this[this.findLastIndex( predicate, context, )];
+	},
+}, );
