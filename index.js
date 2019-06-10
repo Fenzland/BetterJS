98286a17e6c2ef00388efdcc3ed033643cf20e63
Fenz <uukooo@126.com>
2019-06-11 07:18:33 +0800

    better EventTarget:
    {EventTarget}.addEventListener returns the listener
    add {EventTarget}.removeEventListenersByType
    add {EventTarget}.removeAllEventListeners

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import './src/lists-extend-array.js';
 import './src/promisive-timeout-and-animation-frame.js';
 import './src/string-prototype/matchGroup.js';
 import './src/array-prototype/findLastIndex-and-findLast.js';
+import './src/event-target-prototype/listener-control.js';
 import './src/math.js';
---
+++ src/event-target-prototype/listener-control.js
@@ -0,0 +1,73 @@
+
+/**
+ * Changes:
+ * 
+ * {EventTarget}.addEventListener returns the listener.
+ * add {EventTarget}.removeEventListenersByType.
+ * add {EventTarget}.removeAllEventListeners.
+ */
+
+/**
+ * Relationship between target and stored listeners
+ * 
+ * @type {WeakMap}
+ */
+const listeners= new WeakMap();
+
+const addEventListener= EventTarget.prototype.addEventListener;
+const removeEventListener= EventTarget.prototype.removeEventListener;
+
+EventTarget.prototype.addEventListener= function( type, listener, ...options ){
+	const map= listeners.get( this, )|| (map=> (listeners.set( this, map, ), map))( new Map(), );
+	const set= map.get( type, )|| (set=> (map.set( type, set, ), set))( new Set(), );
+	
+	set.add( listener, );
+	
+	addEventListener.call( this, type, listener, ...options, );
+	
+	return listener;
+};
+
+EventTarget.prototype.removeEventListener= function( type, listener, ...options ){
+	const map= listeners.get( this, )|| (map=> (listeners.set( this, map, ), map))( new Map(), );
+	const set= map.get( type, )|| (set=> (map.set( type, set, ), set))( new Set(), );
+	
+	set.delete( listener, );
+	
+	return removeEventListener.call( this, type, listener, ...options, );
+};
+
+Object.defineProperty( EventTarget.prototype, 'removeEventListenersByType', {
+	value( type, ){
+		const map= listeners.get( this, );
+		if(!( map ))
+			return;
+		
+		const set= map.get( type, );
+		if(!( set ))
+			return;
+		
+		set.forEach( listener=> {
+			removeEventListener.call( this, type, listener, );
+		}, );
+		
+		set.clear();
+	},
+}, );
+
+Object.defineProperty( EventTarget.prototype, 'removeAllEventListeners', {
+	value(){
+		const map= listeners.get( this, );
+		if(!( map ))
+			return;
+		
+		map.forEach( ( set, type, )=> {
+			set.forEach( listener=> {
+				removeEventListener.call( this, type, listener, );
+			}, );
+			
+			set.clear();
+		}, );
+	},
+}, );
+
