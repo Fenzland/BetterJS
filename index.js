ed0f226401aeb838a91c291ea781581a6b1c111a
Fenz <uukooo@126.com>
2019-06-12 07:57:57 +0800

    add {Array}.mapAndFilter()

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import './src/lists-extend-array.js';
 import './src/promisive-timeout-and-animation-frame.js';
 import './src/string-prototype/matchGroup.js';
 import './src/array-prototype/findLastIndex-and-findLast.js';
+import './src/array-prototype/mapAndFilter.js';
 import './src/event-target-prototype/listener-control.js';
 import './src/math.js';
---
+++ src/array-prototype/mapAndFilter.js
@@ -0,0 +1,13 @@
+
+Object.defineProperty( Array.prototype, 'mapAndFilter', {
+	value( mapper, filter=(item=> item !== undefined), ){
+		return this.reduce( ( result, item, )=> {
+			const mapped= mapper( item, );
+			
+			if( filter( mapped, ) )
+				result.push( mapped, );
+			
+			return result;
+		}, [], );
+	},
+}, );
