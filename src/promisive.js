f9e64fef0408a1ae516a0aaa30b45266e32e2d93
Fenzland <Fenzland@github.com>
2019-09-03 22:39:23 +0800

    add window.loaded

--- src/promisive.js
+++ src/promisive.js
@@ -18,3 +18,12 @@ globalThis.timeout= ( time, value, )=> new Promise( resolve=> setTimeout( ()=> r
  */
 if( globalThis.requestAnimationFrame )
 	globalThis.nextFrame= value=> new Promise( resolve=> requestAnimationFrame( ()=> resolve( value, ), ), );
+
+/**
+ * window.loaded()
+ *  a window will only load once, so it should be a promise rather than an event
+ * 
+ * @return {Event}  the event of window.onload
+ */
+if( globalThis.window )
+	window.loaded= new Promise( resolve=> window.addEventListener( 'load', resolve, ), );
