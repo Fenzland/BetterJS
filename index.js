36b88b12749dcdcda3d774955abb26136b62020b
Fenzland <Fenzland@github.com>
2019-09-03 22:32:06 +0800

    rename promisive-... to promisive

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import './src/throws.js';
 import './src/instanceof-types.js';
 import './src/lists-extend-array.js';
-import './src/promisive-timeout-and-animation-frame.js';
+import './src/promisive.js';
 import './src/string-prototype/matchGroup.js';
 import './src/string-prototype/toCamelCase.js';
 import './src/string-prototype/find-index.js';
