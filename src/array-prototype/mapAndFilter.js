a77d112bc6bad8cfc4e9171bb85fe32565a7491f
Fenzland <Fenzland@163.com>
2019-10-29 19:04:36 +0800

    {Array}.mapAndFilter() support more parameters

--- src/array-prototype/mapAndFilter.js
+++ src/array-prototype/mapAndFilter.js
@@ -1,10 +1,10 @@
 
 Reflect.defineProperty( Array.prototype, 'mapAndFilter', {
 	value( mapper, filter=(item=> item !== undefined), ){
-		return this.reduce( ( result, item, )=> {
-			const mapped= mapper( item, );
+		return this.reduce( ( result, item, ...rest )=> {
+			const mapped= mapper( item, ...rest, );
 			
-			if( filter( mapped, ) )
+			if( filter( mapped, ...rest, ) )
 				result.push( mapped, );
 			
 			return result;
