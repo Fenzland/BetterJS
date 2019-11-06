828dd024e6b82fac90863aa093e70cd4d15ddf01
Fenzland <Fenzland@163.com>
2019-11-06 11:36:30 +0800

    add Function.isClass()

---
+++ src/function/isClass.js
@@ -0,0 +1,18 @@
+
+/**
+ * check wheather a value is a class
+ * 
+ * @param  value <mixed>
+ * 
+ * @return (boolean)
+ */
+Reflect.defineProperty( Function, 'isClass', {
+	value( value, ){
+		if( typeof value !== 'function' )
+			return false;
+		
+		const code= value.toString();
+		
+		return code.startsWith( 'class', ) || code.startsWith( 'async class', );
+	},
+}, );
