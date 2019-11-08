a0f969448041bbfc74bbdfced145c86e0080e7cb
Fenzland <Fenzland@163.com>
2019-11-08 15:49:46 +0800

    add Function.isFunction(), Function.isAsync()

--- src/function/isXXX.js
+++ src/function/isXXX.js
@@ -16,3 +16,39 @@ Reflect.defineProperty( Function, 'isClass', {
 		return code.startsWith( 'class', ) || code.startsWith( 'async class', );
 	},
 }, );
+
+/**
+ * check whether a value is a function (but not a class)
+ * 
+ * @param  value <mixed>
+ * 
+ * @return (boolean)
+ */
+Reflect.defineProperty( Function, 'isClass', {
+	value( value, ){
+		if( typeof value !== 'function' )
+			return false;
+		
+		const code= value.toString();
+		
+		return !(code.startsWith( 'class', ) || code.startsWith( 'async class', ));
+	},
+}, );
+
+/**
+ * check whether a value is asynchronous
+ * 
+ * @param  value <mixed>
+ * 
+ * @return (boolean)
+ */
+Reflect.defineProperty( Function, 'isAsync', {
+	value( value, ){
+		if( typeof value !== 'function' )
+			return false;
+		
+		const code= value.toString();
+		
+		return code.startsWith( 'async', );
+	},
+}, );
