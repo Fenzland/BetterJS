13bd7d857a0da771e9dbe0a1c164ff7ca60c0931
Fenzland <Fenzland@163.com>
2019-11-12 16:06:45 +0800

    add breaking-free.js only include moduels that not make breaking changes

---
+++ breaking-free.js
@@ -0,0 +1,39 @@
+import './src/SyncPromise.js';
+import './src/expressional-error-control.js'; // DEP: SyncPromise.js
+import './src/promisive.js';
+import './src/getType.js';
+import './src/global-constructors.js';
+import './src/string-prototype/matchGroup.js';
+import './src/string-prototype/toCamelCase.js';
+import './src/string-prototype/find-index.js';
+import './src/array-prototype/forEachAwait.js';
+import './src/array-prototype/get-and-subArray.js';
+import './src/array-prototype/finding.js';
+import './src/array-prototype/mapAndFilter.js';
+import './src/array-prototype/implode-and-feed.js';
+import './src/array-prototype/spliced.js';
+import './src/array-prototype/reversed.js';
+import './src/array-prototype/sorted.js';
+import './src/array-prototype/shuffle-and-shuffled.js';
+import './src/map-and-set/map-from-and-to-object.js';
+import './src/map-and-set/getOrSet.js';
+import './src/map-and-set/pop.js';
+import './src/map-and-set/map.js';
+import './src/map-and-set/flatMap.js';
+import './src/map-and-set/reduce.js';
+import './src/map-and-set/forEachAwait.js';
+import './src/map-and-set/mapAndFilter.js'; // DEP: map-and-set/reduce.js
+import './src/object/isObject-and-isPureObject.js';
+import './src/object/areSame.js'; // DEP: object/isObject-and-isPureObject.js
+import './src/object/deeplyGet.js';
+import './src/object/deeplyAssign.js';
+import './src/object/hasProperty.js';
+import './src/function/isXXX.js';
+import './src/string/compare.js';
+import './src/fp/pipe.js';
+import './src/fp/compose.js';
+import './src/fp/curry.js';
+import './src/promise/any.js';
+import './src/promise/try.js'; // DEP: SyncPromise.js
+import './src/number.js';
+import './src/math.js';
