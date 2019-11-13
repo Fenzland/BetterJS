26ab4f04f11aa0daef6bbe2245f6cfb77d87ed7b
Fenzland <Fenzland@163.com>
2019-11-13 15:31:23 +0800

    {String}.toCamelCase() redesign, remove parameters and be simple

--- src/string-prototype/toCamelCase.js
+++ src/string-prototype/toCamelCase.js
@@ -1,18 +1,15 @@
 
 /**
- * Convert sting to camel case.
- * 
- * @param ucfirst (boolean)  upcase the first character
+ * Convert sting to camelCase.
  * 
  * @return (string)
  */
 Reflect.defineProperty( String.prototype, 'toCamelCase', {
-	value( ucfirst=false, wordDelimiter='_', ){
-		return this.toLowerCase().split( wordDelimiter, ).map( ( word, index, )=> {
-			if( index > 0 || ucfirst )
-				return `${word.slice( 0, 1, ).toUpperCase()}${word.slice( 1, )}`;
-			else
-				return word;
-		}, ).join( '', );
+	value(){
+		return this.split( /(?:[_-]|(?=[A-Z]))/, ).map( ( word, index, )=>
+			index > 0?
+				`${word.slice( 0, 1, ).toUpperCase()}${word.slice( 1, )}`:
+			word.toLowerCase()
+		, ).join( '', );
 	},
 }, );
