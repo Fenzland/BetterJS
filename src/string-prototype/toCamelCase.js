8e719149ed5736515f9c7263a6332761edc89f43
Fenzland <Fenzland@163.com>
2019-11-13 15:35:44 +0800

    add {String}.toFlagCase(), {String}.toSnakeCase(), {String}.toBarbecureCase()

--- src/string-prototype/toCamelCase.js
+++ src/string-prototype/toCamelCase.js
@@ -13,3 +13,39 @@ Reflect.defineProperty( String.prototype, 'toCamelCase', {
 		, ).join( '', );
 	},
 }, );
+
+
+/**
+ * Convert sting to FlagCase.
+ * 
+ * @return (string)
+ */
+Reflect.defineProperty( String.prototype, 'toFlagCase', {
+	value(){
+		return this.split( /(?:[_-]|(?=[A-Z]))/, ).map( ( word, index, )=>
+			`${word.slice( 0, 1, ).toUpperCase()}${word.slice( 1, )}`
+		, ).join( '', );
+	},
+}, );
+
+/**
+ * Convert sting to snake_case.
+ * 
+ * @return (string)
+ */
+Reflect.defineProperty( String.prototype, 'toSnakeCase', {
+	value(){
+		return this.split( /(?:[_-]|(?=[A-Z]))/, ).map( ( word, index, )=> word.toLowerCase(), ).join( '_', );
+	},
+}, );
+
+/**
+ * Convert sting to barbecue-case.
+ * 
+ * @return (string)
+ */
+Reflect.defineProperty( String.prototype, 'toBarbecueCase', {
+	value(){
+		return this.split( /(?:[_-]|(?=[A-Z]))/, ).map( ( word, index, )=> word.toLowerCase(), ).join( '-', );
+	},
+}, );
