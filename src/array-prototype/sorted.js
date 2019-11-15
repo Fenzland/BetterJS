e9cfe6cb2b630a96403b594417bc021d76e8480e
Fenzland <Fenzland@163.com>
2019-11-15 14:32:26 +0800

    fix {Array}.sorted(), support comparer

--- src/array-prototype/sorted.js
+++ src/array-prototype/sorted.js
@@ -3,7 +3,7 @@
  * works like sort, but return a new array rather then modify this array.
  */
 Reflect.defineProperty( Array.prototype, 'sorted', {
-	value(){
-		return [ ...this, ].sort();
+	value( comparer=undefined, ){
+		return [ ...this, ].sort( comparer, );
 	},
 }, );
