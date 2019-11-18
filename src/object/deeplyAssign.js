0d258ba2439d7daff03d35a90ddf8996b8b527fb
Fenzland <Fenzland@163.com>
2019-11-18 17:12:46 +0800

    fix Object.deeplyAssign()

--- src/object/deeplyAssign.js
+++ src/object/deeplyAssign.js
@@ -9,29 +9,34 @@
  */
 Reflect.defineProperty( Object, 'deeplyAssign', {
 	value( target, ...sources ){
-		if(!( typeof target === 'object' && target ))
+		if(!( isObject( target, ) ))
 			target= Object.assign( target, );
 		
 		for( const source of sources )
 		{
-			if(!( typeof source === 'object' && source ))
+			if(!( isObject( source, ) ))
 				return;
 			
 			for( const [ key, value, ] of Object.entries( source, ) )
 			{
-				if(!( typeof value === 'object' && value ))
+				if(!( isObject( value, ) ))
 					target[key]= value;
 				else
 				if( value instanceof Function )
 					target[key]= value;
 				else
 				if( Array.isArray( value, ) )
-					target[key]= Object.deeplyAssign( [], value, );
+					target[key]= Object.deeplyAssign( Array.isArray( target[key], )? target[key]: [], value, );
 				else
-					target[key]= Object.deeplyAssign( {}, value, );
+					target[key]= Object.deeplyAssign( isObject( target[key], )? target[key]: {}, value, );
 			}
 		}
 		
 		return target;
 	},
 }, );
+
+function isObject( value, )
+{
+	return [ 'object', 'function', ].includes( typeof value, ) && value;
+}
