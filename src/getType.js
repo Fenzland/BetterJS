779ce4979118828d44d3c324d790513f9049c263
Fenzland <Fenzland@163.com>
2019-11-20 14:56:13 +0800

    globalThis.getType() add "object" for everything which are objects

--- src/getType.js
+++ src/getType.js
@@ -5,32 +5,32 @@ const betterType= ( value, type, )=>
 	value === null? 'null':
 	type === 'function'? (
 		value.constructor === AsyncFunction? (
-			Function.isClass( value, )? 'async:class':
-			'async:function'
+			Function.isClass( value, )? 'object:async:class':
+			'object:async:function'
 		):
-		value.constructor === GeneratorFunction? 'generator:function':
-		value.constructor === AsyncGeneratorFunction? 'async:generator:function':
-		Function.isClass( value, )? 'class':
-		'function'
+		value.constructor === GeneratorFunction? 'object:generatorfunction:function':
+		value.constructor === AsyncGeneratorFunction? 'object:async:generatorfunction:function':
+		Function.isClass( value, )? 'object:class':
+		'object:function'
 	):
 	type === 'object'? (
-		value instanceof Boolean? 'boolean:object':
-		value instanceof String? 'string:object':
-		value instanceof Number? 'number:object':
-		value instanceof BigInt? 'bigint:object':
-		value instanceof Symbol? 'symbol:object':
-		value instanceof RegExp? 'regexp':
-		value instanceof Promise? 'promise':
-		Array.isArray( value, )? 'array':
-		value instanceof Map? 'map':
-		value instanceof Set? 'set':
-		value instanceof WeakMap? 'weakmap':
-		value instanceof WeakSet? 'weakset':
-		globalThis.WeakRef && value instanceof WeakRef? 'weakref':
+		value instanceof Boolean? 'object:boolean':
+		value instanceof String? 'object:string':
+		value instanceof Number? 'object:number':
+		value instanceof BigInt? 'object:bigint':
+		value instanceof Symbol? 'object:symbol':
+		value instanceof RegExp? 'object:regexp':
+		value instanceof Promise? 'object:promise':
+		Array.isArray( value, )? 'object:array':
+		value instanceof Map? 'object:map':
+		value instanceof Set? 'object:set':
+		value instanceof WeakMap? 'object:weakmap':
+		value instanceof WeakSet? 'object:weakset':
+		globalThis.WeakRef && value instanceof WeakRef? 'object:weakref':
 		value.constructor.constructor? (
 			value.constructor.constructor === AsyncFunction? 'async:object':
-			value.constructor.constructor === GeneratorFunction? 'generator':
-			value.constructor.constructor === AsyncGeneratorFunction? 'async:generator':
+			value.constructor.constructor === GeneratorFunction? 'object:generator':
+			value.constructor.constructor === AsyncGeneratorFunction? 'object:async:generator':
 			'object'
 		):
 		'object'
