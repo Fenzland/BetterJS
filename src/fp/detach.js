b32d9ea3d5509b71be55f2bc3adccbce2746668f
Fenzland <Fenzland@163.com>
2019-11-21 18:26:45 +0800

    fix bug with {Function}.detachTail

--- src/fp/detach.js
+++ src/fp/detach.js
@@ -16,8 +16,8 @@ Reflect.defineProperty( Function.prototype, 'detach', {
 Reflect.defineProperty( Function.prototype, 'detachTail', {
 	value(){
 		const detached= ( ...args )=> this.call(
-			...args.slice( 0, -1, ),
 			...args.slice( -1, ),
+			...args.slice( 0, -1, ),
 		);
 		
 		Reflect.defineProperty( detached, 'length', { value:(this.length - - 1), }, );
