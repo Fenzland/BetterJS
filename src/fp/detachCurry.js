cbe3b56471d384e813d487a55dc1406b01dc87c6
Fenzland <Fenzland@163.com>
2019-11-22 18:42:42 +0800

    rename detachCurry [.detachTail().curry()] to detachTailCurry and and detachCurry [.detach().curry()]

--- src/fp/detachCurry.js
+++ src/fp/detachCurry.js
@@ -1,14 +1,20 @@
 import './detach.js';
 import './curry.js';
 
-Reflect.defineProperty( Function.prototype, 'detachCurry', {
-	value( ...params ){
-		return this.detachTail().curry( ...params, );
-	},
-}, );
-
 Reflect.defineProperty( Function.prototype, 'detachYrruc', {
 	value( ...params ){
 		return this.detach().yrruc( ...params, );
+	},
+}, );
+
+Reflect.defineProperty( Function.prototype, 'detachCurry', {
+	value( ...params ){
+		return this.detach().curry( ...params, );
+	},
+}, );
+
+Reflect.defineProperty( Function.prototype, 'detachTailCurry', {
+	value( ...params ){
+		return this.detachTail().curry( ...params, );
 	},
 }, );
