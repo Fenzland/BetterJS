3aa47170890f17d0dc37604e2b30c5caf57c5e61
Fenzland <Fenzland@github.com>
2019-11-24 11:14:05 +0800

    add test lists-extend-array

---
+++ tests/lists-extend-array.js
@@ -0,0 +1,6 @@
+import { test, } from './Robberfly.js';
+import '../src/lists-extend-array.js';
+
+test( 'lists-extend-array', ()=> {
+	// TODO
+}, );
--- tests/paths.js
+++ tests/paths.js
@@ -3,4 +3,5 @@ export default [
 	'./SyncPromise.js',
 	'./expressional-error-control.js',
 	'./instanceof-types.js',
+	'./lists-extend-array.js',
 ];
