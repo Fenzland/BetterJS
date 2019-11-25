22ccc9814d45773c1182ee844749a9d9f50b4f6b
Fenzland <Fenzland@163.com>
2019-11-25 16:28:48 +0800

    make [ {Map}, {Set}, ].[ entries(), keys(), values(), ] return Arrays

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ import './src/array-prototype/sorted.js';
 import './src/array-prototype/shuffle-and-shuffled.js';
 import './src/map-and-set/map-from-and-to-object.js';
 import './src/map-and-set/better-set-and-add.js';
+import './src/map-and-set/better-entries.js';
 import './src/map-and-set/getOrSet.js';
 import './src/map-and-set/pop.js';
 import './src/map-and-set/map.js';
