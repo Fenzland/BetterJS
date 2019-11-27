b0d76c771472561e451e736b494e5842d07dc5cf
Fenzland <Fenzland@163.com>
2019-11-27 16:31:22 +0800

    add test function-prototype/new

--- tests/paths.js
+++ tests/paths.js
@@ -7,6 +7,7 @@ export default [
 	'./promisive.js',
 	'./global-constructors.js',
 	'./async-function-prototype/limit.js',
+	'./function-prototype/new.js',
 	'./string-prototype/matchGroup.js',
 	'./string-prototype/toXXXCase.js',
 	'./string-prototype/find-index.js',
