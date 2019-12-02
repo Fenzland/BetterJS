7253bb6cb1626fad668425f38d4650e83f19dc96
Fenzland <Fenzland@163.com>
2019-12-02 18:02:30 +0800

    {Function}.through inherit name and length

--- src/fp/through.js
+++ src/fp/through.js
@@ -1,6 +1,11 @@
 
 Reflect.defineProperty( Function.prototype, 'through', {
 	value( walkerIndex=0, ){
-		return ( ...params )=> (this( ...params, ), params.slice( walkerIndex, )[0]);
+		const result= ( ...params )=> (this( ...params, ), params.slice( walkerIndex, )[0]);
+		
+		Reflect.defineProperty( result, 'length', { value:this.length, }, );
+		Reflect.defineProperty( result, 'name', { value:this.name, }, );
+		
+		return result;
 	},
 }, );
