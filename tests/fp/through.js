22b0826ed5763bab19c29fef84a2b0c355af0f77
Fenzland <Fenzland@163.com>
2019-12-02 18:03:00 +0800

    add test about {Function}.through inherit name and length

--- tests/fp/through.js
+++ tests/fp/through.js
@@ -15,6 +15,16 @@ test( 'FP: {Function}.through', async ( { assertBe, assertFunction, }, )=> {
 	assertFunction( to, );
 	assertFunction( tn1, );
 	
+	assertBe( t0.length, func.length, );
+	assertBe( t1.length, func.length, );
+	assertBe( to.length, func.length, );
+	assertBe( tn1.length, func.length, );
+	
+	assertBe( t0.name, func.name, );
+	assertBe( t1.name, func.name, );
+	assertBe( to.name, func.name, );
+	assertBe( tn1.name, func.name, );
+	
 	text='';
 	assertBe( t0( 0, 1, 2, ), 0, );
 	assertBe( text, 'a:0, b:1, c:2', );
