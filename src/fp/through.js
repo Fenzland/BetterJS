0db3414ddaba2e7f074a88b40982e99dd0f76552
Fenzland <Fenzland@163.com>
2019-12-02 18:06:07 +0800

    add {Function}.awaitThrough

--- src/fp/through.js
+++ src/fp/through.js
@@ -1,7 +1,19 @@
+import '../global-constructors.js';
 
 Reflect.defineProperty( Function.prototype, 'through', {
 	value( walkerIndex=0, ){
 		const result= ( ...params )=> (this( ...params, ), params.slice( walkerIndex, )[0]);
+		
+		Reflect.defineProperty( result, 'length', { value:this.length, }, );
+		Reflect.defineProperty( result, 'name', { value:this.name, }, );
+		
+		return result;
+	},
+}, );
+
+Reflect.defineProperty( AsyncFunction.prototype, 'awaitThrough', {
+	value( walkerIndex=0, ){
+		const result= async ( ...params )=> this( ...params, ).then( ()=> params.slice( walkerIndex, )[0] );
 		
 		Reflect.defineProperty( result, 'length', { value:this.length, }, );
 		Reflect.defineProperty( result, 'name', { value:this.name, }, );
