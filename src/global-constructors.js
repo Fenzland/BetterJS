859edad3b622c7dd37e06b763afbc31ec6bcef2c
Fenzland <fenzland@163.com>
2020-01-19 07:48:50 +0800

    global constructor add Generator and AsyncGenerator

--- src/global-constructors.js
+++ src/global-constructors.js
@@ -2,3 +2,6 @@
 Reflect.defineProperty( globalThis, 'AsyncFunction', { value:(async ()=>{}).constructor, }, );
 Reflect.defineProperty( globalThis, 'GeneratorFunction', { value:(function *(){}).constructor, }, );
 Reflect.defineProperty( globalThis, 'AsyncGeneratorFunction', { value:(async function *(){}).constructor, }, );
+
+Reflect.defineProperty( globalThis, 'Generator', { value:(function *(){})().constructor, }, );
+Reflect.defineProperty( globalThis, 'AsyncGenerator', { value:(async function *(){})().constructor, }, );
