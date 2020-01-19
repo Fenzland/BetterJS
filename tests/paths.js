ccd0ea7e1299a9ea4846151b8e8472b09993a48e
Fenzland <fenzland@163.com>
2020-01-19 08:29:54 +0800

    add test async-generator-prototype/forEach

--- tests/paths.js
+++ tests/paths.js
@@ -13,6 +13,7 @@ export default [
 	'./generator-prototype/forEach.js',
 	'./async-generator-prototype/toArray.js',
 	'./async-generator-prototype/map.js',
+	'./async-generator-prototype/forEach.js',
 	'./function-prototype/new.js',
 	'./function-prototype/rename.js',
 	'./string-prototype/matchGroup.js',
