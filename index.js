e8218142006393acf41ed17e9d0267a3d9b23aed
Fenzland <fenzland@163.com>
2020-04-12 21:06:14 +0800

    rename map-and-set/better-set-and-add.js to map-and-set/better-modifications.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ import './src/array-prototype/reversed.js';
 import './src/array-prototype/sorted.js';
 import './src/array-prototype/shuffle-and-shuffled.js';
 import './src/map-and-set/map-from-and-to-object.js';
-import './src/map-and-set/better-set-and-add.js';
+import './src/map-and-set/better-modifications.js';
 import './src/map-and-set/better-entries.js';
 import './src/map-and-set/getOrSet.js';
 import './src/map-and-set/put.js';
