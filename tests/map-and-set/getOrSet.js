07e3fba534845058e1b49245b847be28c2b2b92d
Fenzland <fenzland@163.com>
2020-04-13 19:45:05 +0800

    add tests for {WeakMap}.getOrSet

--- tests/map-and-set/getOrSet.js
+++ tests/map-and-set/getOrSet.js
@@ -51,3 +51,90 @@ test( '{Map}.getOrSet with afterSetting', ( { assertBe, assertAs, assertRun, }, 
 	first.assert( 1, );
 	second.assert( 0, );
 }, );
+
+test( '{WeakMap}.getOrSet', ( { assertBe, assertRun, }, )=> {
+	const map= new WeakMap();
+	const first= assertRun();
+	const second= assertRun();
+	const third= assertRun();
+	const fourth= assertRun();
+	const key= {};
+	const primitive= 'key';
+	
+	const foo= map.getOrSet( key, ()=> {
+		first.run();
+		
+		return 'foo';
+	}, );
+	
+	const bar= map.getOrSet( key, ()=> {
+		second.run();
+		
+		return 'bar'
+	}, );
+	
+	const baz= map.getOrSet( primitive, ()=> {
+		third.run();
+		
+		return 'baz';
+	}, );
+	
+	const qux= map.getOrSet( primitive, ()=> {
+		fourth.run();
+		
+		return 'qux'
+	}, );
+	
+	assertBe( map.has( key, ), true, );
+	assertBe( map.get( key, ), 'foo', );
+	assertBe( map.has( primitive, ), false, );
+	assertBe( foo, 'foo', );
+	assertBe( bar, 'foo', );
+	assertBe( baz, 'baz', );
+	assertBe( qux, 'qux', );
+	
+	first.assert( 1, );
+	second.assert( 0, );
+	third.assert( 1, );
+	fourth.assert( 1, );
+}, );
+
+test( '{WeakMap}.getOrSet with afterSetting', ( { assertBe, assertRun, }, )=> {
+	const map= new WeakMap();
+	const first= assertRun();
+	const second= assertRun();
+	const third= assertRun();
+	const fourth= assertRun();
+	const key= {};
+	const primitive= 'key';
+	
+	const foo= map.getOrSet( key, ()=> 'foo', value=> {
+		first.run();
+		
+		assertBe( value, 'foo', )
+		
+		return value.toUpperCase();
+	}, );
+	
+	const bar= map.getOrSet( key, ()=> 'bar', value=> {
+		second.run();
+	}, );
+	
+	const baz= map.getOrSet( primitive, ()=> 'baz', ()=> {
+		third.run();
+	}, );
+	
+	const qux= map.getOrSet( primitive, ()=> 'qux', ()=> {
+		fourth.run();
+	}, );
+	
+	assertBe( map.has( key, ), true, );
+	assertBe( map.get( key, ), 'foo', );
+	assertBe( foo, 'foo', );
+	assertBe( bar, 'foo', );
+	
+	first.assert( 1, );
+	second.assert( 0, );
+	third.assert( 0, );
+	fourth.assert( 0, );
+}, );
