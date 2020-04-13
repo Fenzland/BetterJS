26f8ca5b09e3958176b757cf9882f0477f13bc76
Fenzland <fenzland@163.com>
2020-04-13 11:50:48 +0800

    map-and-set better modification, throws when necessary if the map or set is frozen, sealed or prevented extensions

--- src/map-and-set/better-modifications.js
+++ src/map-and-set/better-modifications.js
@@ -1,6 +1,11 @@
+import '../object/better-frozen.js';
 
 /**
  * @break-changes
+ * 
+ * Not add or set value with new key when the object is not extensible
+ * Not delete value when the object is sealed
+ * Not set value when the object is frozen
  * 
  * Let set() of Map and WeakMap, add() of Set and WeakSet return the given value.
  */
@@ -8,7 +13,12 @@
 	const origin= Map.prototype.set;
 	
 	Reflect.defineProperty( Map.prototype, 'set', {
-		value( key, value, ){
+		value: function set( key, value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen map', );
+			if( !Object.isExtensible( this, ) && !this.has( key, ) )
+				throw new TypeError( 'cannot add a new key to a non-extensible map', );
+			
 			origin.call( this, key, value, );
 			
 			return value;
@@ -19,7 +29,12 @@
 	const origin= WeakMap.prototype.set;
 	
 	Reflect.defineProperty( WeakMap.prototype, 'set', {
-		value( key, value, ){
+		value: function set( key, value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen weak map', );
+			if( !Object.isExtensible( this, ) && !this.has( key, ) )
+				throw new TypeError( 'cannot add a new key to a non-extensible weak map', );
+			
 			origin.call( this, key, value, );
 			
 			return value;
@@ -30,7 +45,12 @@
 	const origin= Set.prototype.add;
 	
 	Reflect.defineProperty( Set.prototype, 'add', {
-		value( value, ){
+		value: function add( value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen set', );
+			if( !Object.isExtensible( this, ) && !this.has( value, ) )
+				throw new TypeError( 'cannot add a new value to a non-extensible set', );
+			
 			origin.call( this, value, );
 			
 			return value;
@@ -41,10 +61,129 @@
 	const origin= WeakSet.prototype.add;
 	
 	Reflect.defineProperty( WeakSet.prototype, 'add', {
-		value( value, ){
+		value: function add( value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen weak set', );
+			if( !Object.isExtensible( this, ) && !this.has( value, ) )
+				throw new TypeError( 'cannot add a new value to a non-extensible weak set', );
+			
 			origin.call( this, value, );
 			
 			return value;
+		},
+	}, );
+}
+
+{
+	const origin= Map.prototype.delete;
+	
+	Reflect.defineProperty( Map.prototype, 'delete', {
+		value: { delete:function( key, value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen map', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete item from a sealed map', );
+			
+			return origin.call( this, key, value, );
+		}, }.delete,
+	}, );
+}
+{
+	const origin= WeakMap.prototype.delete;
+	
+	Reflect.defineProperty( WeakMap.prototype, 'delete', {
+		value: { delete:function( key, value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen weak map', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete item from a sealed weak map', );
+			
+			return origin.call( this, key, value, );
+		}, }.delete,
+	}, );
+}
+{
+	const origin= Set.prototype.delete;
+	
+	Reflect.defineProperty( Set.prototype, 'delete', {
+		value: { delete:function( value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen set', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete value from a sealed set', );
+			
+			return origin.call( this, value, );
+		}, }.delete,
+	}, );
+}
+{
+	const origin= WeakSet.prototype.delete;
+	
+	Reflect.defineProperty( WeakSet.prototype, 'delete', {
+		value: { delete:function( value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen weak set', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete value from a sealed weak set', );
+			
+			return origin.call( this, value, );
+		}, }.delete,
+	}, );
+}
+
+{
+	const origin= Map.prototype.clear;
+	
+	Reflect.defineProperty( Map.prototype, 'clear', {
+		value: function clear( key, value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen map', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete item from a sealed map', );
+			
+			return origin.call( this, key, value, );
+		},
+	}, );
+}
+{
+	const origin= WeakMap.prototype.clear;
+	
+	Reflect.defineProperty( WeakMap.prototype, 'clear', {
+		value: function clear( key, value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen weak map', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete item from a sealed weak map', );
+			
+			return origin.call( this, key, value, );
+		},
+	}, );
+}
+{
+	const origin= Set.prototype.clear;
+	
+	Reflect.defineProperty( Set.prototype, 'clear', {
+		value: function clear( value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen set', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete value from a sealed set', );
+			
+			return origin.call( this, value, );
+		},
+	}, );
+}
+{
+	const origin= WeakSet.prototype.clear;
+	
+	Reflect.defineProperty( WeakSet.prototype, 'clear', {
+		value: function clear( value, ){
+			if( Object.isFrozen( this, ) )
+				throw new TypeError( 'cannot modify a frozen weak set', );
+			if( Object.isSealed( this, ) )
+				throw new TypeError( 'cannot delete value from a sealed weak set', );
+			
+			return origin.call( this, value, );
 		},
 	}, );
 }
